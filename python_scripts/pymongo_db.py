995ade4dbfde352cf4e797e47cad75fba2605a9e
Erin <ErinLWeir@gmail.com>
2019-09-10 15:19:05 -0700

    Connect PSAW and PRAW scraping to pymongo server to store post data
    Create insert_reddit_submission_dict to insert reddit submissions as documents. Checks if submission doc already exists otherwise inserts.
    
    Scraping reddit now uses generators and PSAW so scraping all posts from beginning of 2019 is possible.
    
    Minor csv update with cleaner gpu names, dropping problematic gpu lines (scientific computing focus, mobile gpus, etc).

--- python_scripts/pymongo_db.py
+++ python_scripts/pymongo_db.py
@@ -1,18 +1,38 @@
 # Module creating and handling local MongosDB
 from pymongo import MongoClient
+import pymongo
+from bson.objectid import ObjectId
 from secrets import mongos_secrets
 
-client = MongoClient(host=mongos_secrets['host'])
-db = client('Hardwarescrape')
+client = MongoClient()
+db = client['Hardwarescrape']
 
 # reddit work - might be refactored into a function to be called
 
-coll = db['reddit']
 
-example_submission_addition = {'_id': 'd2awru',
+example_submission_addition = {'post_id': 'd2awru',
                                'title': '[USA-ME][H] 1080 ROG Strix, Ryzen 7 2700x w/cooler, 16gb Trident Z RGB ram, 16gb Adata XPG RGB ram, MSI B350 Tomahawk [W] Paypal/Local Cash',
                                'self_text': "Hey HWS! I rebuilt my PC about a month ago and I wasn't sure what I was going to do with my old parts. However I've now decided to sell most of them here as they aren't doing me any good just stored away. Everything here I have used and tested and are in great working condition. All Prices are **including shipping** and **negotiable**. If you buy more than one thing I'll cut deals! \n\n[Timestamps](https://imgur.com/a/TywG2pq?fbclid=IwAR1rZt833zXyCvBi17y6qlLuMgzhUPhZtGng8JHsoNgBD4TRFgPjuaZIWdU)\n\n&#x200B;\n\n* GTX 1080 ROG STRIX - $325\n\nUsed this for about 5 months with no overclock or mining done with it. Worked great. Just decided to go with a 2070 Super in the new build\n\n* Ryzen 7 2700x w/ stock prism cooler - $180\n\nUsed this for about 5 months as well with no overclock on it. Again, worked great, just ended up going with Ryzen gen 3 for the new PC. Combo with the MOBO for $240\n\n* MSI B350 Tomahawk MOBO - $75\n\nThis MOBO has it's BIOS updated and worked well with the 2700x. Comes with the IO shield. Combo with the CPU for $240\n\n* 16gb Trident Z RGB ram at 3200mhz - $80\n\nThis set ran and looks great in the PC for the 5 months I used it. Decided to go with a 32gb set of 3600mhz ram with the new PC. \n\n* 16gb Adata XPG RGB ram at 3000mhz - $75\n\nOriginally bought these to use with the old PC but ended up going with the Trident Z set so these have really only been used to test that they work\n\n* Also have an unopened CM Masterfan Pro 140 3 in 1 set - $30\n\nBought this thinking I would use it and never even opened the box. Will combo with other items if interested. \n\n&#x200B;\n\nAll in all these were all from a PC I built 5-6 months ago and my inpatient self saw ryzen 3000 and the super series and impulse bought a new PC. Everything runs great and Like I said prices are plenty negotiable as I don't need them here anymore.",
                                'created': 1568164879.0,
                                'author_info': {'author_id': 't2_r4oab6v',
                                                'author_name': 'daewonmtg',
                                                'author_trade_info': 'Trades: 1'}}
+
+def insert_reddit_submission_dict(submission:dict):
+    """
+    Takes in submission dictionary created from submission object and inserts to mongosdb
+    :param submission: dictionary created from submission via PRAW
+    :return: none
+    """
+    if not submission:
+        return "Error, submission object is None"
+    coll = db['reddit']
+    if coll.find_one({'post_id':submission['_id']}): # _id cannot be a string but has to be an object_id
+        print(f'post_id : {submission.post_id} is already in database!')
+        pass
+    coll.insert_one(submission)
+
+if __name__ == '__main__':
+
+    collection = db['reddit']
+    collection.create_index([("post_id", pymongo.TEXT)], unique=True)
--- python_scripts/subredditscrape.py
+++ python_scripts/subredditscrape.py
@@ -1,7 +1,9 @@
 import praw
+from psaw import PushshiftAPI
 from secrets import reddit_app_key, reddit_secret_key
 import pandas as pd
 from GPUNameScrape import pd_series_to_set
+from pymongo_db import db, insert_reddit_submission_dict
 
 reddit = praw.Reddit(
     client_id=reddit_app_key,
@@ -9,13 +11,59 @@ reddit = praw.Reddit(
     user_agent="HardwareScrape",
 )
 
+psaw_api = PushshiftAPI(reddit)
+
 nvidia_names = pd.read_csv(
     "/home/erin/PycharmProjects/HardwareScrape/data/nvidia_clean_names.csv", index_col=0
 ).rename({"0": "name"}, axis=1)
 gpu_name_set = pd_series_to_set(nvidia_names["name"])
 
 
-def scrape_hws_new(gpu_name_set:set, limit: int=None):
+def scrape_hws_psaw_style(gpu_name_set: set, psaw_api=psaw_api, limit: int = None):
+    """
+    Uses PSAW and PRAW to scrape reddit posts and insert submissions that match criteria into mongodb.
+    :param gpu_name_set: set of strings to search - typically created by pd_series_to_set helper func
+    :param psaw_api: psaw api object
+    :param limit: number of submissions to fetch
+    :return: None, prints number of posts processed
+    """
+    generator = psaw_api.search_submissions(subreddit='hardwareswap', before=1568147665, after=1546300800,
+                                            limit=limit)  # scraping between 9/10/19 13:36 and 1/1/19 00:00
+    posts_processed = 0
+    while True:
+        try:
+            submission_object = next(generator)
+            submission_dict = None
+
+            if submission_object.link_flair_text not in {"SELLING", "CLOSED"}:
+                posts_processed += 1
+                continue
+
+            title = submission_object.title.split()
+            for word in title:
+                if word.lower() in gpu_name_set:
+                    submission_dict = {'_id': submission_object.id,
+                                       'title': submission_object.title,
+                                       'self_text': submission_object.selftext,
+                                       'created': submission_object.created,
+                                       'author_info': {'author_id': submission_object.author_fullname,
+                                                       'author_name': submission_object.author.name,
+                                                       'author_trade_info': submission_object.author_flair_text}
+                                       }
+
+            if not submission_dict:
+                posts_processed +=1
+                continue
+
+            posts_processed += 1
+            insert_reddit_submission_dict(submission_dict)
+
+        except StopIteration:
+            print(f'end of loop, {posts_processed} processed')
+            break
+
+
+def scrape_hws_new(gpu_name_set: set, limit: int = None):
     """
     Goes through /r/hardwareswap/new and checks the title for any words to be in the name set AND is a selling post.
     :param gpu_name_set: Set of GPU model words. nvidia, gtx, 1050 etc.
@@ -35,4 +83,4 @@ def scrape_hws_new(gpu_name_set:set, limit: int=None):
 
 
 if __name__ == "__main__":
-    result_list = scrape_hws_new(gpu_name_set, limit=1000)
+    scrape_hws_psaw_style(gpu_name_set=gpu_name_set, limit=10)
