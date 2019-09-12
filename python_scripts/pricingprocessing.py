c3bd7a05c5564b2a456e9fef53bf883954e85f05
Erin <ErinLWeir@gmail.com>
2019-09-12 13:46:28 -0700

    Restructure regex search and parameters to limit data returned.
    Adjusted regex so posts with 1 price are all that are returned. Getting pricing data from posts with multiple items for sale will be implemented in the future.

--- python_scripts/pricingprocessing.py
+++ python_scripts/pricingprocessing.py
@@ -26,13 +26,12 @@ def pull_pricing_draft(comparison_set: set, bad_set: set, collection_name: str='
         cursor = collection.find(limit=limit)
     else:
         cursor = collection.find()
-    regex = re.compile(r'\$(\d{2,3})+.?(shipp)?(ed|ing)?(OBO)?', re.IGNORECASE) #Searches for $ two or 3 numbers and shipp (ing or ed) or OBO optional.
-
+    # regex_2 = re.compile(r'\$(\d{2,3})+.?(shipp)?(ed|ing)?(OBO)?', re.IGNORECASE) #Searches for $ two or 3 numbers and shipp (ing or ed) or OBO optional.
+    regex = re.compile(r'\$(\d{2,3})+', re.IGNORECASE)
     power_count = 0
     entries_count = 0
+    result_df = pd.DataFrame(columns=['title_select', 'selected_text', 'full_text', 'location_tag', 'post_id', 'author_id', 'author_trades', 'created'])
 
-    empty_df = pd.DataFrame(
-        columns=['title_select', 'selected_text', 'full_text', 'location_tag', 'post_id', 'author_id', 'author_trades', 'created'])
 
     for submission in cursor:
         entries_count +=1
@@ -43,48 +42,71 @@ def pull_pricing_draft(comparison_set: set, bad_set: set, collection_name: str='
         if text.find('removed') != -1 or text.find('deleted') != -1:
             continue
 
+        if len(regex.findall(text)) != 1:
+            continue
+
         match_set, selling_title_substring, power = gpu_likelihood_value(title, comparison_set, bad_set)
 
         if power <= 2:
             continue
 
         power_count +=1
-        low_idx = None
-        high_idx = None
-        for search_term in match_set:
-            potential_idx = text.find(search_term)
-            term_found = potential_idx != -1
-            if term_found and not low_idx:
-                low_idx = potential_idx #set low_idx if it doesn't exist
-            if term_found and low_idx:
-                low_idx = min(term_found, low_idx) #take the left most term
 
-        if not low_idx:
-            low_idx = 0
+        high_idx, low_idx = search_for_price_regex(match_set, regex, text)
 
-        regex_match = regex.search(text, low_idx)
-        if regex_match:
-            high_idx = regex_match.end()
-
-            if low_idx ==0:
-                low_idx = regex_match.start()
-        empty_dict = {'title_select': selling_title_substring,
-                            'selected_text': text[low_idx:high_idx],
-                            'full_text': submission['self_text'],
-                            'location_tag': submission['title'][:8],
-                            'post_id': submission['post_id'],
-                            'author_id': None,
-                            'author_trades': None,
-                            'created': submission['created']}
-        if submission.get('author_info'):
-            empty_dict['author_id'] = submission['author_info']['author_id']
-            empty_dict['author_trades'] = submission['author_info']['author_trade_info']
-
-        empty_df = empty_df.append(empty_dict, ignore_index=True)
+        result_df = append_results_to_df(high_idx, low_idx, selling_title_substring, submission, text, result_df)
 
     print(f'total entries: {entries_count}')
     print(f'entries over power 2: {power_count}')
-    return empty_df
+    return result_df
+
+
+def append_results_to_df(high_idx, low_idx, selling_title_substring, submission, text, df):
+    """
+    Appends parsed results to df as new row
+    :param high_idx:
+    :param low_idx:
+    :param selling_title_substring:
+    :param submission:
+    :param text:
+    :param df:
+    :return:
+    """
+    result_dict = {'title_select': selling_title_substring,
+                  'selected_text': text[low_idx:high_idx],
+                  'full_text': submission['self_text'],
+                  'location_tag': submission['title'][:8],
+                  'post_id': submission['post_id'],
+                  'author_id': None,
+                  'author_trades': None,
+                  'created': submission['created']}
+    if submission.get('author_info'):
+        result_dict['author_id'] = submission['author_info']['author_id']
+        result_dict['author_trades'] = submission['author_info']['author_trade_info']
+    df = df.append(result_dict, ignore_index=True)
+    return df
+
+
+def search_for_price_regex(match_set, regex, text):
+    low_idx = 0
+    high_idx = None
+    # for search_term in match_set:
+    #     potential_idx = text.find(search_term)
+    #     term_found = potential_idx != -1
+    #     if term_found and not low_idx:
+    #         low_idx = potential_idx  # set low_idx if it doesn't exist
+    #     if term_found and low_idx:
+    #         low_idx = min(term_found, low_idx)  # take the left most term
+    # if not low_idx:
+    #     low_idx = 0
+    regex_match = regex.search(text, low_idx)
+    if regex_match:
+        high_idx = regex_match.end()
+
+        # if low_idx == 0:
+        low_idx = regex_match.start()
+    return high_idx, low_idx
+
 
 def gpu_likelihood_value(title: str, comparison_set: set, bad_set: set=None):
     """
