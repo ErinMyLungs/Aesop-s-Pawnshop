abe88d2def4b491377ea953e06586980acc182ab
ErinMyLungs <erinlweir@gmail.com>
2019-11-23 16:38:55 -0800

    Add to req.txt, refactor load_comparison_pickle with new params.
    Add bad_set pickle filepath, add guarding if statements to ensure files exist and make paths relative for easier deployment.

--- python_scripts/pricingprocessing.py
+++ python_scripts/pricingprocessing.py
@@ -1,12 +1,19 @@
 # module for searching and pulling out pricing data from mongo documents
 import pymongo
-from pymongo_db import db
+from .pymongo_db import db
 import re
 import pickle
 from typing import Union
 import pandas as pd
+import os
 
-def pull_pricing_draft(comparison_set: set, bad_set: set, collection_name: str='reddit', limit: Union[None,int]=10):
+
+def pull_pricing_draft(
+    comparison_set: set,
+    bad_set: set,
+    collection_name: str = "reddit",
+    limit: Union[None, int] = 10,
+):
     """
     Goes through collection attempting to pull out pricing data for each entry and discarding entries that are unlikely to have gpus
     :param comparison_set: key terms to match
@@ -21,41 +28,58 @@ def pull_pricing_draft(comparison_set: set, bad_set: set, collection_name: str='
     else:
         cursor = collection.find()
     # regex_2 = re.compile(r'\$(\d{2,3})+.?(shipp)?(ed|ing)?(OBO)?', re.IGNORECASE) #Searches for $ two or 3 numbers and shipp (ing or ed) or OBO optional.
-    regex = re.compile(r'\$(\d{2,3})+', re.IGNORECASE)
+    regex = re.compile(r"\$(\d{2,3})+", re.IGNORECASE)
     power_count = 0
     entries_count = 0
-    result_df = pd.DataFrame(columns=['title_select', 'selected_text', 'full_text', 'full_title', 'location_tag', 'post_id', 'author_id', 'author_trades', 'created'])
-
+    result_df = pd.DataFrame(
+        columns=[
+            "title_select",
+            "selected_text",
+            "full_text",
+            "full_title",
+            "location_tag",
+            "post_id",
+            "author_id",
+            "author_trades",
+            "created",
+        ]
+    )
 
     for submission in cursor:
-        entries_count +=1
+        entries_count += 1
 
-        title = submission['title'].lower()
-        text = submission['self_text'].lower()
+        title = submission["title"].lower()
+        text = submission["self_text"].lower()
 
-        if text.find('removed') != -1 or text.find('deleted') != -1:
+        if text.find("removed") != -1 or text.find("deleted") != -1:
             continue
 
         if len(regex.findall(text)) != 1:
             continue
 
-        match_set, selling_title_substring, power = gpu_likelihood_value(title, comparison_set, bad_set)
+        match_set, selling_title_substring, power = gpu_likelihood_value(
+            title, comparison_set, bad_set
+        )
 
         if power <= 2:
             continue
 
-        power_count +=1
+        power_count += 1
 
         high_idx, low_idx = search_for_price_regex(match_set, regex, text)
 
-        result_df = append_results_to_df(high_idx, low_idx, selling_title_substring, submission, text, result_df)
+        result_df = append_results_to_df(
+            high_idx, low_idx, selling_title_substring, submission, text, result_df
+        )
 
-    print(f'total entries: {entries_count}')
-    print(f'entries over power 2: {power_count}')
+    print(f"total entries: {entries_count}")
+    print(f"entries over power 2: {power_count}")
     return result_df
 
 
-def append_results_to_df(high_idx, low_idx, selling_title_substring, submission, text, df):
+def append_results_to_df(
+    high_idx, low_idx, selling_title_substring, submission, text, df
+):
     """
     Appends parsed results to df as new row
     :param high_idx:
@@ -66,18 +90,20 @@ def append_results_to_df(high_idx, low_idx, selling_title_substring, submission,
     :param df:
     :return:
     """
-    result_dict = {'title_select': selling_title_substring,
-                  'selected_text': text[low_idx:high_idx],
-                  'full_text': submission['self_text'],
-                   'full_title': submission['title'],
-                  'location_tag': submission['title'][:submission['title'].find(']')+1],
-                  'post_id': submission['post_id'],
-                  'author_id': None,
-                  'author_trades': None,
-                  'created': submission['created']}
-    if submission.get('author_info'):
-        result_dict['author_id'] = submission['author_info']['author_id']
-        result_dict['author_trades'] = submission['author_info']['author_trade_info']
+    result_dict = {
+        "title_select": selling_title_substring,
+        "selected_text": text[low_idx:high_idx],
+        "full_text": submission["self_text"],
+        "full_title": submission["title"],
+        "location_tag": submission["title"][: submission["title"].find("]") + 1],
+        "post_id": submission["post_id"],
+        "author_id": None,
+        "author_trades": None,
+        "created": submission["created"],
+    }
+    if submission.get("author_info"):
+        result_dict["author_id"] = submission["author_info"]["author_id"]
+        result_dict["author_trades"] = submission["author_info"]["author_trade_info"]
     df = df.append(result_dict, ignore_index=True)
     return df
 
@@ -103,7 +129,7 @@ def search_for_price_regex(match_set, regex, text):
     return high_idx, low_idx
 
 
-def gpu_likelihood_value(title: str, comparison_set: set, bad_set: set=None):
+def gpu_likelihood_value(title: str, comparison_set: set, bad_set: set = None):
     """
     Creates an int value to weight how likely a post has a gpu based on how many matches in set and how important the match is
     :param title: title of set
@@ -111,27 +137,39 @@ def gpu_likelihood_value(title: str, comparison_set: set, bad_set: set=None):
     :return: the matching words, selling substring, power num tuple
     """
     # TODO: Maybe try splitting title by commas and parse each individually to handle multiitem posts? Future magic
-    selling_title_substring = title[title.find('[h]') + 3: title.find('[w]')]
+    selling_title_substring = title[title.find("[h]") + 3 : title.find("[w]")]
     title_set = set(selling_title_substring.split())
     match_set = title_set & comparison_set
     unmatch_set = title_set & bad_set
     power_num = len(match_set) - len(unmatch_set)
     return match_set, selling_title_substring, power_num
 
-def load_comparison_pickle(filepath: str='/home/erin/PycharmProjects/HardwareScrape/data/comp_set_pickle.P'):
+
+def load_comparison_pickle(
+    comparison_filepath: str = "data/comp_set_pickle.P",
+    bad_filepath: str = "data/bad_set.P",
+):
     """
     loads comparison set pickle from filepath
-    :param filepath:
+    :param comparison_filepath: filepath to comparison pickle, by default in data folder
+    :param bad_filepath: filepath to bad set pickle, default in data folder
     :return: set object
     """
-    with open(filepath, 'rb') as pickle_file:
+    if not os.path.exists(comparison_filepath):
+        raise ValueError(f"Error, filepath {comparison_filepath} does not exist")
+    if not os.path.exists(bad_filepath):
+        raise ValueError(f"Error, filepath {bad_filepath} does not exist")
+
+    with open(comparison_filepath, "rb") as pickle_file:
         comparison_set = pickle.load(pickle_file)
-    with open('/home/erin/PycharmProjects/HardwareScrape/data/bad_set.P', 'rb') as pickle_file:
+
+    with open(bad_filepath, "rb") as pickle_file:
         bad_set = pickle.load(pickle_file)
+
     return comparison_set, bad_set
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     comparison_set, bad_set = load_comparison_pickle()
     # if not comparison_set:
