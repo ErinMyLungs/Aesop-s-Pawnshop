187439f04d3634d21aeed865309e750461e846f2
ErinMyLungs <erinlweir@gmail.com>
2019-11-24 15:45:35 -0800

    Add final pipeline test for front-end data cleanup

--- tests/test_pricingprocessing.py
+++ tests/test_pricingprocessing.py
@@ -6,23 +6,6 @@ import pandas as pd
 import python_scripts.pricingprocessing as pp
 
 
-example_data = {
-    "_id": ObjectId("5d7822e90dcd0567a40c23d3"),
-    "post_id": "d2dwp1",
-    "title": "[USA-TX][H] Zotac GTX 1660 Ti AMP [W] Paypal, Local Cash",
-    "self_text": "Like new, around 3 months old. Looking to upgrade so gotta get rid of this boy. Never overclocked.\n\nLooking for 255 + shipping\n\n250 cash local\n\nTimestamps: [https://imgur.com/gallery/u29F36V](https://imgur.com/gallery/u29F36V)\n\nLocal is 76129, but I don't have a car so you would have to be able to make it to Texas Christian University.\n\nI haven't registered it for warranty so that still may be possible with a new owner.",
-    "created": 1568176182.0,
-    "author_info": {
-        "author_id": "t2_3z50nip",
-        "author_name": "jguy2000",
-        "author_trade_info": None,
-    },
-}
-
-
-# model: 1660, is_ti: True, price: 255, trades: 0, created: 1568176182, post_id: d2dwp1
-
-
 class TestFrontendCleaning:
     final_result_df = pd.read_csv("tests/test_resources/example_pp_df.csv", index_col=0)
 
@@ -146,3 +129,9 @@ class TestFrontendCleaning:
 
         with pytest.raises(ValueError):
             pp.extract_trades_info(test_df)
+
+    def test_front_end_data_cleanup(self):
+        test_df = self.starter_df.copy(deep=True)
+        test_df = pp.front_end_data_cleanup(test_df)
+
+        pd.testing.assert_frame_equal(test_df, self.final_result_df)
