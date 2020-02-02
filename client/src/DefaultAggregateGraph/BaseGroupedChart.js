224f6a5a504569157bdaa3ede4a00d48bd8b27f9
ErinMyLungs <erinlweir@gmail.com>
2020-02-02 15:43:02 -0800

    Fix x-axis labels misalignment and refactor component state with better data structure.
    Minor format update on y-axis tick labels to include dollar sign. Data now follows a much more legible format that avoids need to map values together before display.

--- client/src/DefaultAggregateGraph/BaseGroupedChart.js
+++ client/src/DefaultAggregateGraph/BaseGroupedChart.js
@@ -6,27 +6,46 @@ class BaseGroupedChart extends Component {
         super(props);
         this.state = {
             //Placeholder data TODO: Hook into flask api
-            models: ['760', '780', '950', '960', '970', '980', '1030', '1050', '1060', '1070', '1080', '1650', '1660', '2060', '2070', '2080'],
-            non_ti: [69, 107, 56, 81, 102, 201, 64, 103, 154, 240, 347, 132, 168, 315, 430, 610],
-            ti: [0, 110, 0, 0, 0, 235, 0, 104, 0, 285, 494, 0, 221, 0, 0, 562],
+
+            data : [
+                {model: '760', nonTiPrice:69, tiPrice:0},
+                {model: '780', nonTiPrice:107, tiPrice:110},
+                {model: '950', nonTiPrice:56, tiPrice:0},
+                {model: '960', nonTiPrice:81, tiPrice:0},
+                {model: '970', nonTiPrice:102, tiPrice:0},
+                {model: '980', nonTiPrice:201, tiPrice:235},
+                {model: '1030', nonTiPrice:64, tiPrice:0},
+                {model: '1050', nonTiPrice:103, tiPrice:104},
+                {model: '1060', nonTiPrice:154, tiPrice:0},
+                {model: '1070', nonTiPrice:240, tiPrice:285},
+                {model: '1080', nonTiPrice:347, tiPrice:494},
+                {model: '1650', nonTiPrice:132, tiPrice:0},
+                {model: '1660', nonTiPrice:168, tiPrice:221},
+                {model: '2060', nonTiPrice:315, tiPrice:0},
+                {model: '2070', nonTiPrice:430, tiPrice:0},
+                {model: '2080', nonTiPrice:610, tiPrice:562},
+            ],
+
         }
     }
 
     render() {
-        // let ticks = Array.from(this.state.models.keys(), x => x+1); TODO: Possibly use to refactor for consistent label placement?
         return (
             <div
                 className={'base-grouped-chart'}
                 style={{display: 'flex'}}
             >
+
                 <VictoryChart
                     domainPadding={{x: 10}}
                 >
+{/*TODO: Add legend*/}
                     <VictoryAxis
                         dependentAxis={true}
                         label={"Average Price ($USD"}
+                        tickFormat={(Price) => `$${Price}`}
                         style={{
-                            axisLabel: {fontSize: 10, padding: 20},
+                            axisLabel: {fontSize: 10, padding: 30},
                             tickLabels: {fontSize: 7, padding: 5}
                         }}
                     />
@@ -34,6 +53,7 @@ class BaseGroupedChart extends Component {
                     <VictoryAxis
                         dependentAxis={false}
                         label={"GPU Models (Arranged by Generation)"}
+                        tickLabelComponent={<VictoryLabel dy={-3}/>}
                         style={{
                             axisLabel: {fontSize: 10, padding: 30, angle: 0},
                             tickLabels: {fontSize: 7, padding: 10, angle: -90}
@@ -47,15 +67,15 @@ class BaseGroupedChart extends Component {
                     >
 
                         <VictoryBar
-                            data={this.state.models.map((e, i) => [e, this.state.non_ti[i]])}
-                            x={0}
-                            y={1}
+                            data={this.state.data}
+                            x={"model"}
+                            y={"tiPrice"}
                         />
 
                         <VictoryBar
-                            data={this.state.models.map((e, i) => [e, this.state.ti[i]])}
-                            x={0}
-                            y={1}
+                            data={this.state.data}
+                            x={'model'}
+                            y={"nonTiPrice"}
                         />
 
                     </VictoryGroup>
